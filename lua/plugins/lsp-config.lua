f043728674f8b99fafc7036c6facd1ca49b17278
Chance Crosson <chancecrosson@gmail.com>
2026-05-31 12:44:27 -0500

    Updated telescope

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -130,8 +130,7 @@ return {
   -- Telescope is a fuzzy finder file browser.
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    branch = '0.1.x',
+    version = '*',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope-file-browser.nvim',
